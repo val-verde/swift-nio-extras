a2890245711564a8100e867b13b0e4214e6a4e36
Kartheek Solipuram <kartheek.s@gmail.com>
2020-10-25 19:32:01 -0700

    Update swift nio package ref to val-verde.

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         .library(name: "NIOHTTPCompression", targets: ["NIOHTTPCompression"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.9.0"),
+        .package(url: "https://github.com/val-verde/swift-nio.git", .branch("dutch-master")),
     ],
     targets: targets
 )
